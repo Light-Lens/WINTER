ca0bb1a1ea60028ca787b0b018a2579984ab4a50
Light-Lens <QCoreNest@gmail.com>
2024-03-16 02:05:28 +0530

    Test with first two skills

---
+++ main.py
@@ -0,0 +1,22 @@
+from src.vendor.GATw.src.alphabet.classification.train import Train
+from src.vendor.GATw.src.alphabet.classification.sample import Sample
+
+t = Train(
+    n_layer = 8,
+    n_hidden = 8,
+    lr = 1e-3,
+    batch_size = 32,
+    model = "RNN"
+)
+t.preprocess("data\\skills.json", metadata=("skills", "skill", "patterns"), data_division=None)
+t.train(
+    n_steps = 4000,
+    eval_interval = 400,
+    eval_iters = 800
+)
+t.save("bin\\skills.pth")
+
+s = Sample("bin\\skills.pth")
+s.load()
+print(s.predict("mute"))
+print(s.predict("unmute"))
