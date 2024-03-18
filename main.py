b8c51a4501558219999829285ad352c5f7da6dc7
Light-Lens <QCoreNest@gmail.com>
2024-03-18 11:56:09 +0530

    Add 2 more tests

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from src.vendor.GATw.src.alphabet.classification.train import Train
 from src.vendor.GATw.src.alphabet.classification.sample import Sample
 
-# t = Train(
-#     n_layer = 2,
-#     n_hidden = 8,
-#     lr = 1e-3,
-#     batch_size = 32,
-#     model = "RNN"
-# )
-# t.preprocess("data\\skills.json", metadata=("skills", "skill", "patterns"), data_division=None)
-# t.train(
-#     n_steps = 1000,
-#     eval_interval = 100,
-#     eval_iters = 400
-# )
-# t.save("bin\\skills.pth")
+t = Train(
+    n_layer = 3,
+    n_hidden = 8,
+    lr = 1e-3,
+    batch_size = 32,
+    model = "RNN"
+)
+t.preprocess("data\\skills.json", metadata=("skills", "skill", "patterns"), data_division=None)
+t.train(
+    n_steps = 2000,
+    eval_interval = 200,
+    eval_iters = 400
+)
+t.save("bin\\skills.pth")
 
 s = Sample("bin\\skills.pth")
 s.load()
@@ -28,7 +28,9 @@ test = [
     "set volume one hundred percent.",
     "set the volume to zero percent.",
     "mute my pc for a minute",
-    "unmute my PC now."
+    "unmute my PC now.",
+    "increase the volume of this PC.",
+    "please increase the volume"
 ]
 
 for i in test:
