08c7125d5effe46d5690a402165299ec9833b5ae
Light-Lens <QCoreNest@gmail.com>
2024-03-21 13:45:46 +0530

    Update GATw

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 from src.vendor.GATw.src.alphabet.classification.train import Train
 
 t = Train(
-    n_layer = 3,
+    n_layer = 4,
     n_hidden = 8,
-    lr = 1e-3,
-    batch_size = 32,
+    lr = 1e-4,
+    batch_size = 64,
     model = "RNN"
 )
 t.preprocess("data\\skills.json", metadata=("skills", "skill", "patterns"), data_division=None)
 t.train(
-    n_steps = 2000,
-    eval_interval = 200,
-    eval_iters = 400
+    n_steps = 7000,
+    eval_interval = 350,
+    eval_iters = 700,
+    n_loss_digits = 7
 )
 t.save("bin\\skills.pth")
