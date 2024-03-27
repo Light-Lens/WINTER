026282a09ad09f2c7a794588492ea6d6749e5bc6
Light-Lens <QCoreNest@gmail.com>
2024-03-27 18:26:13 +0530

    Remove AOs as submodule

--- src/WINTER/core/TTS.py
+++ src/WINTER/core/TTS.py
@@ -1,24 +1,13 @@
-# Text To Speech
+# https://github.com/rany2/edge-tts.git
+from ..shared.utils import get_bin_path
 import playsound, asyncio, edge_tts, os
-
-def get_cache_path():
-    # Get the absolute path of the current file
-    current_file_path = os.path.abspath(__file__)
-
-    # Navigate up the directory tree four times
-    parent_dir = current_file_path
-    rel_current_path = os.path.relpath(__file__)
-    for _ in range(len(rel_current_path.split(os.sep))):
-        parent_dir = os.path.dirname(parent_dir)
-
-    # Append "cache" directory to the parent directory
-    return os.path.join(parent_dir, "bin\\cache")
 
 # Speak out loud the text
 def Speak(text, voice="en-US-GuyNeural"):
-    output = os.path.join(get_cache_path(), "audio.mp3")
-    if not os.path.isdir(get_cache_path()):
-        os.mkdir(get_cache_path())
+    cache_path = os.path.join(get_bin_path(), "cache")
+    output = os.path.join(cache_path, "audio.mp3")
+    if not os.path.isdir(cache_path):
+        os.mkdir(cache_path)
 
     # TTS engine
     async def _main(text, voice):
